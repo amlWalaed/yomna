47e17bf35257b3dfbcebcb65a8f3e8d62517d6b2
aml walaed <amlwalaed@gmail.com>
2023-12-23 18:07:20 +0200

    add questions

--- controllers/QuestionsController.php
+++ controllers/QuestionsController.php
@@ -1,10 +1,15 @@
 <?php
  require '../config/db.php';
-// if($_SERVER['REQUEST_METHOD'] ==='GET'){
-    // }
-        $query = 'SELECT * FROM questions';
-        $statement = $conn->prepare($query);
-        $result = $statement->execute();
-        return json_encode($statement->fetchObject());
+ header("Content-Type: application/json; charset=UTF-8");
+if($_SERVER['REQUEST_METHOD'] ==='GET'){
+    $query = 'SELECT * FROM questions';
+    $statement = $conn->prepare($query);
+    $result = $statement->execute();
+    echo json_encode($statement->fetchAll(PDO::FETCH_CLASS));
+    }
 
+if($_SERVER['REQUEST_METHOD'] ==='POST'){
+    
+        var_dump($_POST);
+    }
 ?>
--- pages/questions.php
+++ pages/questions.php
@@ -1,24 +1,72 @@
-<?php
-require '../controllers/QuestionsController.php'
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Student | Test </title>
+    <link rel="stylesheet" href="../css/tailwind.css" />
 </head>
 <body>
-    <main class="grid place-items-center bg-emerald-500">
-        
+    <main class="grid p-12 place-items-center bg-emerald-100">
+        <form method="post" action="../controllers/QuestionsController.php" class="w-full max-w-sm p-6 space-y-6 border rounded shadow text-emerald-700 bg-black/10">
+               <section id="questions-card" class="space-y-6">
+           
+              </section>
+              <button type="submit">Send</button>
+        </form>
 
     </main>
     <script>
-        try{
-            fetch('../controllers/QuestionsController.php').then((res)=>res.text()).then(res=>console.log(res))
-        }catch(e){
-            throw Error(e.message)
+        let currentQuestion = 0;
+        let questions =[]
+        
+        document.addEventListener('DOMContentLoaded', function(){
+            try{
+                fetch('../controllers/QuestionsController.php').then((res)=> res.json()
+                ).then((response)=>{
+                    
+                   questions = response
+                   createQuestion()
+                })
+            }catch(e){
+                throw Error(e.message)
+            }
+        })
+        const createQuestion = () => {
+           let questionsCard =document.querySelector('#questions-card')
+         questions.forEach((question)=>{
+             
+             let questionCard = document.createElement('div')
+              let questionContainer = document.createElement('h2')
+              questionContainer.innerHTML =question.question
+              questionContainer.classList.add('question')
+              questionCard.appendChild(questionContainer)
+
+              let choosesContainer = document.createElement('div')
+              let chooses = JSON.parse(question.chooses)
+              for(index in chooses ){
+                 let chooseContainer = document.createElement('div')
+                 let radioInput = document.createElement('input')
+                 let Inputlabel = document.createElement('label')
+
+                 Inputlabel.setAttribute('for',index)
+                 Inputlabel.textContent = chooses[index]
+                 radioInput.setAttribute('type', 'radio')
+                 radioInput.setAttribute('id', index)
+                 radioInput.setAttribute('name',question.id )
+                 radioInput.setAttribute('value', index)
+                 chooseContainer.appendChild(radioInput)
+                 chooseContainer.appendChild(Inputlabel)
+                 choosesContainer.appendChild(chooseContainer)
+              }
+              questionCard.appendChild(choosesContainer)
+              questionsCard.appendChild(questionCard)
+
+
+         })
         }
+       
     </script>
 </body>
 </html>
